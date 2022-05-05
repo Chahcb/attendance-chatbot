020dd6a68d778bd1f24e530e7cba134c1e1dd736
Charline bch <charline.bouche@epsi.fr>
2022-05-05 10:55:33 +0200

    add documentation

--- BOT-001.py
+++ BOT-001.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -7,3 +8,18 @@ app = FastAPI()
 async def read_item(name):
     response = "Hello" + name
     return {response}
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Attendance chatbot",
+        version="1.0",
+        description="Chatbot de présence à la cantine",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
