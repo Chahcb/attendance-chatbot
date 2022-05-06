29239c481495972a2b82361fbbaf90e642c8a005
Tarcaye <thomas.carpaye@gmail.com>
2022-05-06 16:07:01 +0200

    BOT-004 : Add path parameter to hello endpoint
    * path parameter /hello/name
    * add documentation
    
    Co-authored-by: Charline bch <charline.bouche@epsi.fr>

--- BOT-001.py
+++ BOT-001.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello "}
+@app.get("/hello/{name}")
+async def read_item(name):
+    response = "Hello" + name
+    return {response}
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Attendance chatbot",
+        version="1.0",
+        description="Chatbot de présence à la cantine",
+        routes=app.routes,
+    )
+
+    # affiche le logo en haut à gauche
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
