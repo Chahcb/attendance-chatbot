654c27854f49c443bc18c894c218e060ad544e64
Charline bch <charline.bouche@epsi.fr>
2022-05-13 11:08:25 +0200

    Refractor create_attendance

--- BOT_controller.py
+++ BOT_controller.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from pydantic import BaseModel
-import unittest
 
 description = """
 BOT pour simplifier l'écriture de la présence à la cantine dans le groupe google chat Présence 🚀
@@ -47,36 +46,12 @@ async def read_name(name):
 @app.post("/attendance", tags=["Présence à la cantine"])
 async def create_attendance(info_attendance: Attendance):
     name = info_attendance.displayName
-    presence = []
+    presence = ""
 
     for i in range(len(info_attendance.attendance)):
-        presence.append(info_attendance.attendance[i])
-        presence.append("|")
-        affichage_presence = ""
-        for word in presence:
-            affichage_presence += str(word)
-        affichage_presence = affichage_presence.rstrip(affichage_presence[-1])
-    return {"text : ", name, " : ", affichage_presence}
-
-
-presence = Attendance(displayName="Charline", attendance="ooooo")
-dict_info_presence = presence.dict()
-
-
-class test_attendance(unittest.TestCase):
-
-    def full_attendance(self):
-        return "ooooo"
-
-    def no_attendance(self):
-        return "xxxxx"
-
-    def test_attendance(self):
-        if self.assertEqual("ooooo", self.full_attendance()):
-            return "full présence"
-
-        elif self.assertEqual("xxxxx", self.no_attendance()):
-            return "No présence"
+        presence += info_attendance.attendance[i] + "|"
+    presence = presence.rstrip(presence[-1])
+    return {"text : ", name, " : ", presence}
 
 
 def custom_documentation():
